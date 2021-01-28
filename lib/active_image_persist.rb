a74c8f815d87327fc3aa05df779eebbfc090b225
wonghockchuansugianto <sugi@gengosha.co.jp>
2021-01-28 15:49:16 +0900

    initail commit

---
+++ lib/active_image_persist.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "active_image_persist/version"
+
+module ActiveImagePersist
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/active_image_persist/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module ActiveImagePersist
+  VERSION = "0.1.0"
+end
