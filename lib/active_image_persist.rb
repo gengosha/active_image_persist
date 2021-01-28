4ce786f2a09587b0343123bd9bae6321bdd8a540
wonghockchuansugianto <sugi@gengosha.co.jp>
2021-01-28 17:31:43 +0900

    up

--- lib/active_image_persist.rb
+++ lib/active_image_persist.rb
@@ -6,26 +6,78 @@ module ActiveImagePersist
   class Error < StandardError; end
   # Your code goes here...
 
-    def attach_images_to_meta_tag
-    if meta_tag_params[:og_image].present?
-      @meta_tag.og_image.purge if @meta_tag.og_image.attached?
-      @meta_tag.og_image.attach(meta_tag_params[:og_image])
-    elsif cookies[:meta_tag_og_image].present?
-      @meta_tag.og_image.purge if @meta_tag.og_image.attached?
-      @meta_tag.og_image.attach(ActiveStorage::Blob.find_by(key: cookies[:meta_tag_og_image]))
-    end
+  # def new
+  #   delete_cache
+  # end
 
-    delete_image_cookies
+  # def edit
+  #   delete_cache
+  # end
+
+  # def create
+  #   if save
+  #     attach_images_to @obj, params
+  #   else
+  #     persist_img
+  #   end
+  # end
+
+  # def update
+  #   if update
+  #     attach_images_to @obj, params
+  #   else
+  #     persist_img
+  #   end
+  # end
+
+  def attach_img_to(obj, params)
+    return if @keys.blank? || params != Hash || obj.blank?
+    @keys.each do |k|
+      if params[k].present?
+        obj.call(k.to_s).purge if obj.call(k).attached?
+        obj.call(k.to_s).attach(params[k])
+      elsif cookies[:active_image_persist][k].present?
+        obj.call(k.to_s).purge if obj.call(k.to_s).attached?
+        obj.call(k.to_s).attach(ActiveStorage::Blob.find_by(key: cookies[:active_image_persist][k]))
+      end
+    end
+    delete_cache
   end
 
-  def save_attachment_keys_as_cookies
-    if meta_tag_params[:og_image].present?
-      og_image = ActiveStorage::Blob.create_and_upload!(io: meta_tag_params[:og_image], filename: meta_tag_params[:og_image].original_filename)
-      cookies[:meta_tag_og_image] = og_image.key
+  def persist_img params
+    return if @keys.blank? || params != Hash
+    @keys.each do |k|
+      if params[k].present?
+        file = params[k]
+        img = ActiveStorage::Blob.create_and_upload!(io: file, filename: file.original_filename)
+        cookies['active_image_persist'][k] = img
+      end
     end
   end
 
-  def delete_image_cookies
-    cookies.delete :meta_tag_og_image
+  def delete_cache
+    return if @keys.blank?
+    @keys.each do |k|
+      cookies.delete ['active_image_persist'][k]
+    end
+  end
+
+  def persisted_img(k=nil, klass='', style='')
+    blob = ActiveStorage::Blob.find_by(key: cookies['active_image_persist'][k])
+    unless blob.blank?
+      image_tag(blob, style: style, class: klass)
+    else
+      nil
+    end
+  end
+
+  def clean_unassociated_blobs
+    ids = ActiveStorage::Blob.all.select{|e| e.attachments.blank? }.pluck(:id)
+    ActiveStorage::Blob.where(id: unassociated_blob_ids).each {|e| e.purge }
+  end
+
+  def setup_persist_img arr
+    return if arr.blank? || arr != Array
+    @keys = arr
   end
 end
--- lib/active_image_persist/version.rb
+++ lib/active_image_persist/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module ActiveImagePersist
-  VERSION = "0.1.1"
+  VERSION = "1.0.2"
 end
