ee4afbd8f03017fef8c36437ad1a5dfe255a96fe
wonghockchuansugianto <sugi@gengosha.co.jp>
2021-02-03 18:47:36 +0900

    eval is evil

--- lib/active_image_persist.rb
+++ lib/active_image_persist.rb
@@ -3,20 +3,26 @@ module ActiveImagePersist
   included do
     helper_method :persisted_img
 
-    def attach_img_to(obj, params)
-      return if @keys.blank? || !params.is_a?(ActionController::Parameters) || obj.blank?
+    def attach_img_to(_obj, params)
+      return if @keys.blank? || !params.is_a?(ActionController::Parameters) || _obj.blank?
       begin
         @keys.each do |k|
           s = k.to_s.gsub('{:', '').gsub('_attributes=>:', '.').gsub('}', '')
+          obj = _obj
           file = k.is_a?(Hash) ? params[k.first[0].to_s][k.first[1].to_s] : params[k]
           cache = k.is_a?(Hash) ? cookies["#{k.first[0].to_s}_#{k.first[1].to_s}".to_sym] : cookies[k]
+          if k.is_a?(Hash)
+            s_ar = s.split('.')
+            obj = _obj.send(s_ar[0])
+            s = s_ar[1]
+          end
 
           if file.present?
-            eval("obj.#{s}.purge if obj.#{s}.attached?")
-            eval("obj.#{s}.attach(file)")
+            obj.send(s).purge if obj.send(s).attached?
+            obj.send(s).attach(file)
           elsif cache.present?
-            eval("obj.#{s}.purge if obj.#{s}.attached?")
-            eval("obj.#{s}.attach(ActiveStorage::Blob.find_by(key: cache))")
+            obj.send(s).purge if obj.send(s).attached?
+            obj.send(s).attach(ActiveStorage::Blob.find_by(key: cache))
           end
         end
       rescue; end
